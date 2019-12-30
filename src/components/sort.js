6733b51e90d3141e45164c2bd409a233d4b7afc6
stormdice <stormdice23@gmail.com>
2019-12-30 11:49:52 +0500

    Сделал сортировку

--- src/components/sort.js
+++ src/components/sort.js
@@ -9,7 +9,7 @@ export const SortType = {
 const getSortTemplate = () => {
   return (/* html */
     `<ul class="sort">
-      <li><a href="#" data-sort-type="${SortType.DEFAULT}" class="sort__button">Sort by default</a></li>
+      <li><a href="#" data-sort-type="${SortType.DEFAULT}" class="sort__button sort__button--active">Sort by default</a></li>
       <li><a href="#" data-sort-type="${SortType.DATE}" class="sort__button">Sort by date</a></li>
       <li><a href="#" data-sort-type="${SortType.RATING}" class="sort__button">Sort by rating</a></li>
     </ul>`
