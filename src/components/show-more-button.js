68c391d1649654350f06233adf7eacf5d5a1eb69
stormdice <stormdice23@gmail.com>
2020-01-31 11:47:37 +0500

    Разбивает файлы на модули

---
+++ src/components/show-more-button.js
@@ -0,0 +1,9 @@
+/**
+ * Создаёт и возвращает разметку кнопки 'Смотреть больше'
+ * @return {string}
+ */
+export const createShowMoreButtonTemplate = () => {
+  return (
+    `<button class="films-list__show-more">Show more</button>`
+  );
+};
