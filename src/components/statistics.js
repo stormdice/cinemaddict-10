5d1382fb6b78124929caa16a6dc867e79f78b6e6
stormdice <stormdice23@gmail.com>
2020-03-10 14:23:34 +0500

    Добавляет библиотеку chart.js и плагин к ней chartjs-plugin-datalabels, показывает в статистике пример из документации библиотеки

--- src/components/statistics.js
+++ src/components/statistics.js
@@ -1,4 +1,46 @@
 import AbstractComponent from "./abstract-component.js";
+import Chart from 'chart.js';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+
+
+const renderChart = (ctx) => {
+  return new Chart(ctx, {
+    type: `bar`,
+    data: {
+      labels: [`Red`, `Blue`, `Yellow`, `Green`, `Purple`, `Orange`],
+      datasets: [{
+        label: `# of Votes`,
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          `rgba(255, 99, 132, 0.2)`,
+          `rgba(54, 162, 235, 0.2)`,
+          `rgba(255, 206, 86, 0.2)`,
+          `rgba(75, 192, 192, 0.2)`,
+          `rgba(153, 102, 255, 0.2)`,
+          `rgba(255, 159, 64, 0.2)`
+        ],
+        borderColor: [
+          `rgba(255, 99, 132, 1)`,
+          `rgba(54, 162, 235, 1)`,
+          `rgba(255, 206, 86, 1)`,
+          `rgba(75, 192, 192, 1)`,
+          `rgba(153, 102, 255, 1)`,
+          `rgba(255, 159, 64, 1)`
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  });
+};
 
 const createStatisticsTemplate = () => {
   return (
@@ -52,7 +94,21 @@ const createStatisticsTemplate = () => {
 };
 
 export default class Statistics extends AbstractComponent {
+  constructor() {
+    super();
+
+    this._renderCharts();
+  }
+
   getTemplate() {
     return createStatisticsTemplate();
+  }
+
+  _renderCharts() {
+    const element = this.getElement();
+
+    const ctx = element.querySelector(`.statistic__chart`);
+
+    this._genresChart = renderChart(ctx);
   }
 }
