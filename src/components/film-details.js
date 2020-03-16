7ee0ea650f86813cddf903c6ca030e7c8023d3f9
stormdice <stormdice23@gmail.com>
2020-03-17 00:36:23 +0500

    создаёт разметку персонального рейтинга, и отображает проставленный рейтинг

--- src/components/film-details.js
+++ src/components/film-details.js
@@ -4,6 +4,8 @@ import {formatRuntime, filmDetailsFormatReleaseDate} from '../utils/common.js';
 import {EMOTIONS} from '../const.js';
 import he from 'he';
 
+const RATING_SCORE = 9;
+
 const createGenreMarkup = (genres) => {
   return Array.from(genres)
     .map((filmGenre) => {
@@ -14,8 +16,37 @@ const createGenreMarkup = (genres) => {
     .join(`\n`);
 };
 
-const createUserRatingMarkup = (film) => {
-  const {title, poster} = film;
+const createScoreTemplate = (count, isChecked = false) => {
+  return (
+    `<input
+      type="radio"
+      name="score"
+      class="film-details__user-rating-input  visually-hidden"
+      value="${count}"
+      id="rating-${count}"
+      ${isChecked ? `checked` : ``}
+    >
+    <label class="film-details__user-rating-label" for="rating-${count}">${count}</label>`
+  );
+};
+
+const createRatingInputs = (rating) => {
+  let score = 0;
+
+  return new Array(RATING_SCORE)
+    .fill(``)
+    .map(() => {
+      const checked = score === rating ? true : false;
+
+      score++;
+
+      return createScoreTemplate(score, checked);
+    })
+    .join(`\n`);
+};
+
+const createUserRatingMarkup = (title, poster, personalRating) => {
+  const scoreTemplate = createRatingInputs(personalRating);
 
   return (
     `<div class="form-details__middle-container">
@@ -34,35 +65,7 @@ const createUserRatingMarkup = (film) => {
 
             <p class="film-details__user-rating-feelings">How you feel it?</p>
 
-            <div class="film-details__user-rating-score">
-              <input type="radio" name="score" class="film-details__user-rating-input visually-hidden" value="1" id="rating-1">
-              <label class="film-details__user-rating-label" for="rating-1">1</label>
-
-              <input type="radio" name="score" class="film-details__user-rating-input visually-hidden" value="2" id="rating-2">
-              <label class="film-details__user-rating-label" for="rating-2">2</label>
-
-              <input type="radio" name="score" class="film-details__user-rating-input visually-hidden" value="3" id="rating-3">
-              <label class="film-details__user-rating-label" for="rating-3">3</label>
-
-              <input type="radio" name="score" class="film-details__user-rating-input visually-hidden" value="4" id="rating-4">
-              <label class="film-details__user-rating-label" for="rating-4">4</label>
-
-              <input type="radio" name="score" class="film-details__user-rating-input visually-hidden" value="5" id="rating-5">
-              <label class="film-details__user-rating-label" for="rating-5">5</label>
-
-              <input type="radio" name="score" class="film-details__user-rating-input visually-hidden" value="6" id="rating-6">
-              <label class="film-details__user-rating-label" for="rating-6">6</label>
-
-              <input type="radio" name="score" class="film-details__user-rating-input visually-hidden" value="7" id="rating-7">
-              <label class="film-details__user-rating-label" for="rating-7">7</label>
-
-              <input type="radio" name="score" class="film-details__user-rating-input visually-hidden" value="8" id="rating-8">
-              <label class="film-details__user-rating-label" for="rating-8">8</label>
-
-              <input type="radio" name="score" class="film-details__user-rating-input visually-hidden" value="9" id="rating-9" checked="">
-              <label class="film-details__user-rating-label" for="rating-9">9</label>
-
-            </div>
+            <div class="film-details__user-rating-score">${scoreTemplate}</div>
           </section>
         </div>
       </section>
@@ -122,6 +125,7 @@ const createFilmDetailsTemplate = (film) => {
     releaseDate,
     runtime,
     description,
+    personalRating,
     isWatchlist,
     isWatched,
     isFavorite,
@@ -209,7 +213,7 @@ const createFilmDetailsTemplate = (film) => {
           </section>
         </div>
 
-        ${isWatched ? createUserRatingMarkup(film) : ``}
+        ${isWatched ? createUserRatingMarkup(title, poster, personalRating) : ``}
 
         <div class="form-details__bottom-container">
           <section class="film-details__comments-wrap">
