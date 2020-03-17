81225682d0d0121e5941e23c46659ae4624dea11
stormdice <stormdice23@gmail.com>
2020-03-17 18:27:36 +0500

    Добавляет модель-адаптер для комментария

---
+++ src/models/comment.js
@@ -0,0 +1,17 @@
+export default class Comment {
+  constructor(data) {
+    this.id = data[`id`];
+    this.author = data[`author`];
+    this.text = data[`comment`];
+    this.date = data[`date`];
+    this.emotion = data[`emotion`];
+  }
+
+  static parseComment(data) {
+    return new Comment(data);
+  }
+
+  static parseComments(data) {
+    return data.map(Comment.parseComment);
+  }
+}
