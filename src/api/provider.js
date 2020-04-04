15c0625ff00e2a22bbd2f169be5b28cb501a48cf
stormdice <stormdice23@gmail.com>
2020-04-04 13:44:05 +0500

    Добавит в провайдер проверку на синхронизацию

--- src/api/provider.js
+++ src/api/provider.js
@@ -56,6 +56,10 @@ export default class Provider {
     return this._api.deleteComment(id);
   }
 
+  getSynchronize() {
+    return this._isSynchronized;
+  }
+
   _isOnLine() {
     return window.navigator.onLine;
   }
